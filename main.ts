5b1c51ba77486560ad5ddcd2094d22b506d43c8e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-11-03 08:14:17 +0900

    feat: implement basic function

---
+++ main.ts
@@ -0,0 +1,6 @@
+export const newName = (prefix: string): string => {
+  if (prefix.length > 30) {
+    throw new Error("prefix must be less than 30 characters");
+  }
+  return `${prefix}${Array.from({ length: 50 - prefix.length }, () => Math.floor(Math.random() * 36).toString(36)).join("")}`;
+};
